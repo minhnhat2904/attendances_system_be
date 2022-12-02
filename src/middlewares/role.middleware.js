78b20157a6e30b81609cad2df1ec73c29deb7e18
Minh Nhat <phancaominhnhat10@gmail.com>
2022-12-02 23:53:15 +0700

    fixed api

--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -1,7 +1,6 @@
 import { HttpError, checkRoleAndPermision } from "../utils";
 
 const checkPermission = (permissionCode) => async (req, res, next) => {
-    console.log(id);
     try {
 		const hasPermission = await checkRoleAndPermision(id, permissionCode);
         if (!hasPermission) {
